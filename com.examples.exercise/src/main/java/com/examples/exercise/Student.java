e9145a831d1939775f7b5c967de5b2fb2e37af2a
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-11-10 15:21:33 +0100

    modify Student with name and id

--- com.examples.exercise/src/main/java/com/examples/exercise/Student.java
+++ com.examples.exercise/src/main/java/com/examples/exercise/Student.java
@@ -1,5 +1,21 @@
 package com.examples.exercise;
 
 public class Student {
+	private String name;
+	private String id;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 	
 }
